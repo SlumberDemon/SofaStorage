4ae40d83fd879ca50bd649605891a042b885a691
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-27 13:50:38 +0100

    Add create and login

--- src/sofa.py
+++ src/sofa.py
@@ -1,12 +1,48 @@
+from .key import KEY
 from deta import Deta
 from typing import Union
 
 class SofaStorage:
-    def __init__(self, drive: Deta.Drive):
+    def __init__(self, drive: Deta.Drive, silent: bool = False):
         self.drive = drive
+        self.silent = silent
 
     def __repr__(self):
         return f"<SofaStorage>"
 
     def __log(self, prompt: str) -> None:
-        print(prompt)
+        if not self.silent:
+            print(prompt)
+
+    @classmethod
+    def create(cls, username: str, password: str, base: str = None, silent: bool = False):
+        key = base if base else KEY
+        if len(username) < 5:
+            raise ValueError("Use at least 5 characters!")
+        if password == KEY:
+            raise ValueError("Don't use project key as password!")
+        if len(password) < 8:
+            raise ValueError("Use at least 8 characters!")
+        if username == password:
+            raise ValueError("Username and password can't be the same!")
+        try:
+            base = Deta(Key).Base(f'{username}_{password}')
+            if not silent:
+                print(f"Account ({username}) created!")
+                return cls(base=base, silent=silent)
+        except AssertionError:
+            raise ValueError("Used an invalid login token!")
+
+    @classmethod
+    def login(cls, username: str, password: str, base: str = None, silent: bool = False):
+        key = base if base else KEY
+        try:
+            base = Deta(key).Base(f'{username}_{password}')
+            if not silent:
+                print(f"Logged in as ({username})")
+                print('-------')
+                return cls(base=base, silent=silent)
+            else:
+                raise Exception(f"Account ({username}) doesn't exist!")
+        except AssertionError:
+            raise ValueError("Used an invalid login token!")
