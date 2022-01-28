614980a7dc50073bcbded9cbd2db7f3ed5ec2927
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-28 12:31:40 +0100

    fixed items fetching

--- src/sofastorage/sofa.py
+++ src/sofastorage/sofa.py
@@ -73,8 +73,8 @@ class SofaStorage:
             if website:
                 fetch = self.base.fetch({'website': website})
             for item in fetch.items:
-                print(f'[•] | ' + item['username'] + ' | ' + item['password'] + ' | ' + item['website'] + ' | ')            
-                return print(f'[•] Found ({fetch.count}) results')
+                print(f'[•] ' + item['username'] + ' | ' + item['password'] + ' | ' + item['website'] + ' | ')            
+            return print(f'[•] Found {fetch.count} result(s)')
         except:
             raise Exception('Missing website or username search query!')
 
