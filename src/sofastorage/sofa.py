8091c9b452b91be395b54ae9deb2d81a127a7661
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-28 08:21:58 +0100

    added validation to create

--- src/sofastorage/sofa.py
+++ src/sofastorage/sofa.py
@@ -19,10 +19,22 @@ class SofaStorage:
 
     @classmethod
     def create(cls, username: str, password: str, silent: bool = False):
-        base = Deta(KEY).Base(f'{username}-{password}')
-        storage = base.put({'item':'.sofa'}, key='.sofa')
-        sofa = base.get(key='.sofa')
-        if sofa:
-            print(f'Logged in as {username}')
-        if not silent:
-            print(f'Account ({username}) created')
+        if len(username) < 5:
+            raise ValueError("Use at least 5 characters!")
+        if password == KEY:
+            raise ValueError("Don't use project key as password!")
+        if len(password) < 8:
+            raise ValueError("Use at least 8 characters!")
+        if username == password:
+            raise ValueError("Username and password can't be the same!")
+        try:
+            base = Deta(KEY).Base(f'{username}-{password}')
+            sofa = base.get(key='.sofa')
+            if sofa:
+                print(f'Logged in as {username}')
+            if not silent:
+                print(f'Account ({username}) created!')
+            storage = base.put({'item':'.sofa'}, key='.sofa')
+            return cls(base=base, silent=silent)
+        except:
+            raise ValueError("Used an invalid login token!")
